54b0959763ab6c5c558182fc9e30ce2b656fd745
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-12 21:06:58 +0530

    Shivi | update redis starter version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("com.robotutor:robotutor-tech-utils:1.0.27")
     implementation("com.robotutor:web-client-starter:1.0.5")
     implementation("com.robotutor:kafka-starter:1.0.8")
-    implementation("com.robotutor:redis-starter:1.0.19")
+    implementation("com.robotutor:redis-starter:1.0.20")
 
     testImplementation("com.robotutor:robotutor-tech-utils:1.0.11:tests")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
